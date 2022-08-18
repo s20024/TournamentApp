17b8796ebb9ef309b503f5db2f4c995c3f452e2a
s20024 <s20024@std.it-college.ac.jp>
2022-08-18 16:47:07 +0900

    indexページの初期を追加した。

---
+++ js/header.js
@@ -0,0 +1,6 @@
+const ham = document.getElementById("btn_hamburger")
+ham.addEventListener('click', () => {
+  const nav = document.getElementById("nav")
+  ham.classList.toggle("active")
+  nav.classList.toggle("active")
+})
