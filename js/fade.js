7f45fcf855f111ef4bec1c70ffb2f5805cd44314
s20024 <s20024@std.it-college.ac.jp>
2022-08-24 15:01:39 +0900

    画像の訂正、URLの訂正、Fade機能の実装

--- js/fade.js
+++ js/fade.js
@@ -1,5 +1,7 @@
 console.log("load fade.js")
 
+let fade_contents = true
+
 const allHeight = Math.max(
   document.body.scrollHeight, document.documentElement.scrollHeight,
   document.body.offsetHeight, document.documentElement.offsetHeight,
@@ -8,7 +10,21 @@ const allHeight = Math.max(
 const mostBottom = allHeight - window.innerHeight
 window.addEventListener('scroll', ()=> {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop
-    if (scrollTop >= mostBottom) {
+    if (scrollTop >= mostBottom && fade_contents) {
       console.log("scroll down")
+      fade_contents = false
+      createContents("fade_out_div", "contents_contents")
     }
 })
+
+async function createContents(from, to) {
+  const contents = document.getElementById(to)
+  const fade_out_div = document.getElementById(from)
+  const children = fade_out_div.children
+  const len = children.length
+  for (let i = 0; i < len; i++) {
+    const child = children[0]
+    await new Promise(s => setTimeout(s, 500))
+    contents.appendChild(child)
+  }
+}
