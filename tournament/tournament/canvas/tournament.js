b1a5f7f45cafec4dc354f57d3cf77247c002aeda
s20024 <s20024@std.it-college.ac.jp>
2022-10-11 17:00:18 +0900

    トーナメント表の作成

--- tournament/tournament/canvas/tournament.js
+++ tournament/tournament/canvas/tournament.js
@@ -3,41 +3,209 @@ import TournamentCell from "./tournament/tournamentCell.js"
 
 class Can {
   constructor() {
-    this.tourtal_group_count = document.group_count
-    this.data = document.data
-    this.group = document.group
-    const canvas = document.getElementById("tournament")
-    this.canvas = canvas
-    this.ctx = canvas.getContext("2d")
   }
 
   start() {
-    this.tourtal_group_count = document.group_count
+    const tourtal_group_count = document.group_count
+    this.tourtal_group_count = tourtal_group_count
     this.data = document.data
-    this.group = document.group
+    const group = document.group
+    this.group = group
+    const howEx = this.howExponentiation(group.length, 0)
+    this.howEx = howEx
+
+    const canvas = document.getElementById("tournament")
+    this.canvas = canvas
+    const ctx = canvas.getContext("2d")
+    this.ctx = ctx
+
+    const canvasWidth = window.innerWidth * 0.45
+    const canvasHeight = window.innerHeight * 0.5
+    this.canvasWidth = canvasWidth
+    this.canvasHeight = canvasHeight
+
+    this.cameraOffset = {
+      x: canvasWidth / 2,
+      y: canvasHeight / 2
+    }
+
+    const cameraZoom = 0.5
+    this.cameraZoom = cameraZoom
+    this.MAX_ZOOM = 5
+    this.MIN_ZOOM = 0.1
+    this.SCROLL_SENSITIVITY = 0.0005
+
+    this.isDragging = false
+    this.dragStart = {x: 0, y: 0}
+    this.initialPinchdistance = null
+    this.lastZoom = cameraZoom
+
+    const trophy_image = new Image()
+    trophy_image.src = "../../images/trophy_gold.png"
+    this.trophy_image = trophy_image
+
+
+    this.canvas_groups = group.map((group_data, index) => {
+      return new TournamentCell(350, 100, tourtal_group_count, index, group_data.name, group_data.id, ctx, howEx, 0)
+    })
+
+    document.canvas_groups = this.canvas_groups
+
+    canvas.addEventListener("mousedown",this.onPointerDown.bind(this))
+    canvas.addEventListener("touchstart",(e) => this.handleTouch(e, this.onPointerDown.bind(this)))
+    canvas.addEventListener("mouseup", this.onPointerUp.bind(this))
+    canvas.addEventListener("touchend", (e) => this.handleTouch(e, this.onPointerUp.bind(this)))
+    canvas.addEventListener("mousemove", this.onPointerMove.bind(this))
+    canvas.addEventListener("touchmove", (e) => this.handleTouch(e, this.onPointerMove.bind(this)))
+    canvas.addEventListener("wheel", (e) => this.adjustZoom(e.deltaY * this.SCROLL_SENSITIVITY))
+    canvas.addEventListener("wheel", (e) => {e.preventDefault()})
+    addEventListener("resize", this.resize.bind(this))
+
+    this.draw()
+  }
+
+  resize() {
+    console.log("resize")
+    const canvasWidth = window.innerWidth * 0.45
+    const canvasHeight = window.innerHeight * 0.5
+
+    this.canvasWidth = canvasWidth
+    this.canvasHeight = canvasHeight
+  }
+
+  draw() {
+    const group = this.group
     const canvas = this.canvas
     const ctx = this.ctx
-    const group = this.group
-  
-    const group_count = document.getElementById("group_cover_contents").children.length
+    const canvasWidth = this.canvasWidth
+    const canvasHeight = this.canvasHeight
+    const cameraZoom = this.cameraZoom
+    const cameraOffset = this.cameraOffset
+    const group_count = group.length
+    canvas.width = canvasWidth
+    canvas.height = canvasHeight
 
-    group.forEach((group_data, index) => {
-      if (index % 2 === 0) {
-        ctx.fillRect(60 + (160 * index) + (100 / 2) - 3, 350 - 100, 83, 6)
-        if (Math.floor(index / 2) % 2 === 0) {
-          ctx.fillRect(60 + (160 * index) + (100 / 2) - 3 + 80, 350 - 100 - 100, 6, 100)
-          ctx.fillRect(60 + (160 * index) + (100 / 2) - 3 + 80, 350 - 100 - 100, 160, 6)
-        } else {
-          ctx.fillRect(60 + (160 * index) + (100 / 2) - 3 + 80, 350 - 100 - 100, 6, 100)
-          ctx.fillRect(60 + (160 * index) + (100 / 2) - 3 - 80, 350 - 100 - 100, 160, 6)
-        }
-      } else {
-        ctx.fillRect(60 + (160 * index) + (100 / 2) - 80, 350 - 100, 83, 6)
-      }
-      ctx.fillRect(60 + (160 * index) + (100 / 2) - 3, 350 - 100, 6,(100 / 2) + 100 )
-      const testCell = new TournamentCell(350, 100, this.tourtal_gourp_count, index, group_data.name, group_data.id, ctx)
-      testCell.create()
+    ctx.translate(canvasWidth / 2, canvasHeight / 2)
+    ctx.scale(cameraZoom, cameraZoom)
+    ctx.translate(-canvasWidth / 2 + cameraOffset.x, -canvasHeight / 2 + cameraOffset.y)
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+    // ここから表示の関数
+    this.drawLine(this.howEx, this.howEx, 0, 0)
+    this.canvas_groups.forEach(cell => {
+      cell.create()
     })
+    ctx.drawImage(this.trophy_image, -50, -50, 100, 100)
+    // ここから表示の関数
+    requestAnimationFrame( this.draw.bind(this))
+  }
+
+  drawLine(count, now, x, y) {
+    const ctx = this.ctx
+    const width =  2 ** (now - 1) * 160
+    const half = width / 2
+    ctx.fillStyle = "#000000"
+    ctx.fillRect(x - 2, y, 4, 125)
+    if (now !== 0) {
+      ctx.fillRect(x - half, y + 125, width, 4)
+      this.drawLine(count, now - 1, x - half, y + 125)
+      this.drawLine(count, now - 1, x + half, y + 125)
+      return
+    } else {
+      return
+    }
+  }
+
+  howExponentiation(x, c) {
+    if (parseInt(x / 2) === 0) {
+      return c
+    } else {
+      return this.howExponentiation(x / 2, c + 1)
+    }
+  }
+
+  getEventLocation(e) {
+    if (e.touches && e.touches.length == 1) {
+      return {x: e.touches[0].clientX, y: e.touches[0].clientY}
+    } else if (e.clientX && e.clientY) {
+      return {x: e.clientX, y: e.clientY}
+    }
+  }
+
+  drawRect(x, y, width, height) {
+    this.ctx.fillRect(x, y, width, height)
+  }
+  drawText(text, x, y, size, font) {
+    const ctx = this.ctx
+    ctx.font = `${size}px ${font}`
+    ctx.fillText(text, x, y)
+  }
+
+  onPointerDown(e) {
+    const cameraZoom = this.cameraZoom
+    const cameraOffset = this.cameraOffset
+    this.isDragging = true
+    this.dragStart.x = this.getEventLocation(e).x / cameraZoom - cameraOffset.x
+    this.dragStart.y = this.getEventLocation(e).y / cameraZoom - cameraOffset.y
+  }
+
+  onPointerUp(e) {
+    const cameraZoom = this.cameraZoom
+    this.isDragging = false
+    this.initialPinchDistance = null
+    this.lastZoom = cameraZoom
+  }
+
+  onPointerMove(e) {
+    const isDragging = this.isDragging
+    const cameraZoom = this.cameraZoom
+    const dragStart = this.dragStart
+    if (isDragging) {
+      this.cameraOffset.x = this.getEventLocation(e).x / cameraZoom - dragStart.x
+      this.cameraOffset.y = this.getEventLocation(e).y / cameraZoom - dragStart.y
+    }
+  }
+
+  handleTouch(e, singleTouchHandler) {
+    if (e.touches.length === 1) {
+      singleTouchHandler(e)
+    } else if (e.type === "touchmove" && e.touches.length === 2) {
+      this.isDragging = false
+      this.handlePinch(e)
+    }
+  }
+
+  handlePinch(e) {
+    e.preventDefault()
+    const touch1 = {x: e.touches[0].clientX, y: e.touches[0].clientY}
+    const touch2 = {x: e.touches[1].clientX, y: e.touches[1].clientY}
+
+    const currentDistance = (touch1.x - touch2.x) ** 2 + (touch1.y - touch2.y) ** 2
+
+    const initialPinchDistance = this.initialPinchDistance
+    if (initialPinchDistance === null) {
+      this.initialPinchDistance = currentDistance
+    } else {
+      this.adjustZoom(null, currentDistance / initialPinchDistance)
+    }
+  }
+
+  adjustZoom(zoomAmount, zoomFactoor) {
+    const isDragging = this.isDragging
+    if (!isDragging) {
+      if (zoomAmount) {
+        this.cameraZoom += zoomAmount
+      } else if (zoomFactor) {
+        const lastZoom = this.lastZoom
+        this.cameraZoom = zoomFactor * lastZoom
+      }
+
+      const MAX_ZOOM = this.MAX_ZOOM
+      const MIN_ZOOM = this.MIN_ZOOM
+      const cameraZoom = this.cameraZoom
+      this.cameraZoom = Math.min(cameraZoom, MAX_ZOOM)
+      this.cameraZoom = Math.max(cameraZoom, MIN_ZOOM)
+    }
   }
 }
 
--- tournament/tournament/canvas/tournament/tournamentCell.js
+++ tournament/tournament/canvas/tournament/tournamentCell.js
@@ -1,5 +1,5 @@
 class TournamentCell {
-  constructor(height, size, tourtal_group_count, group_number, group_name, group_id, ctx) {
+  constructor(height, size, tourtal_group_count, group_number, group_name, group_id, ctx, howEx, winCount) {
     this.height = height
     this.size = size
     this.tourtal_group_count = tourtal_group_count
@@ -9,11 +9,42 @@ class TournamentCell {
     this.group_name = group_name
     this.ctx = ctx
     this.image = document.getElementById(this.group_tagid)
+    this.howEx = howEx
+    this.now = howEx
+    this.winCount = winCount
   }
 
   create() {
     const ctx = this.ctx
-    ctx.drawImage(this.image, 60 + 160 * this.group_number, this.height, this.size, this.size)
+    const group_number = this.group_number
+    const size = this.size
+    const half = size / 2
+
+    const x = this.calcX(this.howEx, this.now, group_number)
+    const y = this.calcY(this.howEx)
+
+    ctx.drawImage(this.image, x - half, y - half, size, size)
+  }
+
+  calcX(howEx, now, group_number) {
+    const by = group_number.toString(2).padStart(howEx, "0")
+    let c = 0
+    for (let i = 0; i < (howEx - this.winCount); i++) {
+      if (by[i] === "1") {
+        c = c + 2 ** (howEx - (i + 1))
+      } else {
+        c = c - 2 ** (howEx - (i + 1))
+      }
+    }
+    return c * 80
+  }
+
+  calcY(howEx) {
+    return (howEx + 1 - this.winCount) * 125
+  }
+
+  setWinCount(winCount) {
+    this.winCount = winCount
   }
 }
 
