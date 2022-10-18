8ed0d4bd32d7df5b1e008cbf35bed68db9d3c100
s20024 <s20024@std.it-college.ac.jp>
2022-10-18 11:00:24 +0900

    てすと

--- js/url.js
+++ js/url.js
@@ -1,1 +1,2 @@
-this.gasurl = "https://script.google.com/macros/s/AKfycbxOoQVoCb6NmwsbNHtgDGDndDqfAuBQfn8chCH8fOlo5IZud1wWim6zotiprBdS3ST-/exec"
+// this.gasurl = "https://script.google.com/macros/s/AKfycbxOoQVoCb6NmwsbNHtgDGDndDqfAuBQfn8chCH8fOlo5IZud1wWim6zotiprBdS3ST-/exec"
+this.gasurl = "https://script.google.com/macros/s/AKfycbxD0ZtAslsupTZOaUkGUyMxKQk_w-q7POgVmxU83aut5_9WpUsDwSDwdUKx3bnnfV-L/exec"
